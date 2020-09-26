6cb7abb5ce634026e28abb740600b8a48959fd35
Nick Smedira <nsmedira@me.com>
2020-09-26 08:30:15 -0400

    disable content div on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,17 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
-import dogSilhouette from "../videos/dog-silhouette.mp4"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="background-video-container">
-      <video id="background-video" autoPlay muted loop>
-        <source src={dogSilhouette} type="video/mp4"/>
-      </video>
-    </div>
-    <div class="content">
+
+    {/* <div class="content">
       <h1>Welcome to Hunting Dog Films</h1>
       <p>Lorem ipsum...</p>
-      {/* Use a button to pause/play the video with JavaScript */}
       <button id="myBtn" onclick="myFunction()">Pause</button>
-    </div>
+    </div> */}
+
   </Layout>
 )
 
