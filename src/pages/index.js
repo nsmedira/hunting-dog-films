15b2c7a124519dc38af4bd68bbcd9cce23f62f09
Nick Smedira <nsmedira@me.com>
2020-09-27 17:07:03 -0400

    build out index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,61 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
+import M from 'materialize-css'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage = () => {
 
-    {/* <div class="content">
-      <h1>Welcome to Hunting Dog Films</h1>
-      <p>Lorem ipsum...</p>
-      <button id="myBtn" onclick="myFunction()">Pause</button>
-    </div> */}
+  useEffect(() => {
+    M.AutoInit()
+  })
 
-  </Layout>
-)
+  return (  
+    <Layout>
+      <SEO title="Home" />
+
+      {/* <div class="content">
+        <h1>Welcome to Hunting Dog Films</h1>
+        <p>Lorem ipsum...</p>
+        <button id="myBtn" onclick="myFunction()">Pause</button>
+      </div> */}
+
+      <div className="container">
+
+        <div className="coming-soon">
+
+          <div className="row">
+            <div className="col s12">
+              <h2 className="hunting-dog-font">Coming soon to a city near you:</h2>
+              <h1 
+                className="hunting-dog-font"
+                // style={{
+                //   marginBottom: 0,
+                // }}
+              >
+                The Hunting Dog Film Tour
+              </h1>
+            </div>
+          </div>
+
+          {/* ENTER EMAIL FOR UPDATES */}
+          <div className="row">
+            <form className="col s12">
+              <div class="row">
+                <h3 className="hunting-dog-font">Enter your email for updates about the tour!</h3>
+                <div class="input-field col s12">
+                  <input id="email" type="email" class="validate" />
+                  <label htmlFor="email">Enter your email</label>
+                </div>
+              </div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+
+    </Layout>
+  )
+}
 
 export default IndexPage
