7d947eeb108efb8a308a2446992c05cfcca06149
Nick Smedira <nsmedira@me.com>
2020-09-29 19:41:40 -0400

    import navbar component

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,41 +1,16 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Navbar from "../nav/navbar"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle, pathname }) => (
   <header
     style={{
       background: `transparent`,
       marginBottom: `1.45rem`,
-      marginTop: "8vh"
+      // marginTop: "8vh"
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 
-        id="site-title" 
-        className="hunting-dog-font" 
-        style={{ 
-          textAlign: "center"        
-        // margin: 0,
-        }}
-      >
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <Navbar pathname={pathname}/>
   </header>
 )
 
