3077a1be6e2f02eeac11ebb4d5e84648533e718b
Nick Smedira <nsmedira@me.com>
2020-09-29 23:58:04 -0400

    add content from billy

--- src/pages/about.js
+++ src/pages/about.js
@@ -31,6 +31,12 @@ const About = () => {
             </Link>
         </h1>
 
+        <div className="paragraph">
+            <p className="flow-text">My name is Billy White, founder of Hunting Dog Films/The Hunting Dog Film Tour.  I started this company with a mission to provide high quality content focused on hunting dogs, their owners and the stories that we love to hear about about our 4 legged friends.  We, hunting dog owners, might pursue different game each year, but in the end we do it to watch our dogs do what they were born to do and learn some of life’s most important lessons from our friends that cannot say a word.  Thus, Hunting Dog Films was born.</p>
+            
+            <p className="flow-text">From pit-mixes in kevlar chasing pigs to mountain curs treeing squirrels we want to hear about your dog and their best stories from each Fall. Send us pictures, videos, and some text that can help paint a picture of what makes your dog so special.  We look forward to hearing from you!</p>
+        </div>
+
         </div>
 
       </div>
--- src/pages/tour.js
+++ src/pages/tour.js
@@ -31,6 +31,12 @@ const Tour = () => {
               </Link>
             </h1>
             
+            <div className="paragraph">
+                <p className="flow-text">It’s not the size of the dog in the fight, but the size of the fight in the dog.</p> 
+                <p className="flow-text">The Hunting Dog Film Tour is a submission-based film festival highlighting hunting dogs, their owners and the stories made afield. Modeled off the successful Hunting and Fly Fishing Film tours, we figured everyone likes to show their dogs off and who isn’t a sucker for hunting dog stories…however fabricated they might be.</p>
+                <p className="flow-text">If you have a great hunting dog and story, we want to see it and share it on the big screen.</p>
+            </div>
+
         </div>
 
       </div>
