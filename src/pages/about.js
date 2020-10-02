1b8816b62c1f4946c66c11764c4426470d5fcb97
nsmedira <nsmedira@me.com>
2020-10-02 06:41:56 -0400

    change page title and remove 'coming-soon' box

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from 'gatsby'
 import SEO from "../components/seo"
 
 const About = () => {
@@ -10,8 +9,6 @@ const About = () => {
 
       <div className="container">
 
-        <div className="coming-soon">
-
         <h1 
             id="site-title" 
             className="hunting-dog-font" 
@@ -20,23 +17,13 @@ const About = () => {
             // margin: 0,
             }}
         >
-            <Link
-            to="/"
-            style={{
-                color: `white`,
-                textDecoration: `none`,
-            }}
-            >
-            About Page
-            </Link>
+            About
         </h1>
 
         <div className="paragraph">
-            <p className="flow-text">My name is Billy White, founder of Hunting Dog Films/The Hunting Dog Film Tour.  I started this company with a mission to provide high quality content focused on hunting dogs, their owners and the stories that we love to hear about about our 4 legged friends.  We, hunting dog owners, might pursue different game each year, but in the end we do it to watch our dogs do what they were born to do and learn some of life’s most important lessons from our friends that cannot say a word.  Thus, Hunting Dog Films was born.</p>
+            <p className="flow-text">My name is Billy White, founder of Hunting Dog Films/The Hunting Dog Film Tour.  I started this company with a mission to provide high quality content focused on hunting dogs, their owners and the stories that we love to hear about our 4 legged friends.  We, hunting dog owners, might pursue different game each year, but in the end we do it to watch our dogs do what they were born to do and learn some of life’s most important lessons from our friends that cannot say a word.  Thus, Hunting Dog Films was born.</p>
             
             <p className="flow-text">From pit-mixes in kevlar chasing pigs to mountain curs treeing squirrels we want to hear about your dog and their best stories from each Fall. Send us pictures, videos, and some text that can help paint a picture of what makes your dog so special.  We look forward to hearing from you!</p>
-        </div>
-
         </div>
 
       </div>
