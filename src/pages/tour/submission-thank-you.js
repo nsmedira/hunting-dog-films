73a9bd9d068d2c805cb21f53de0206c9c50554c7
Nick Smedira <nsmedira@me.com>
2020-10-07 14:54:51 -0400

    create submission thank you page

---
+++ src/pages/tour/submission-thank-you.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SubmissionThankYou = () => {
+
+    return (
+
+        <div className="container">
+
+            <h1 className="hunting-dog-font">Thank you for your film submission!</h1>
+            <p>Be on the lookout for a confirmation email! We will be in touch shortly with more information.</p>
+
+        </div>
+
+    )
+
+}
+
+export default SubmissionThankYou
