cab70b5cd6f4920e6147060b462a67b3a13c20bb
Nick Smedira <nsmedira@me.com>
2020-10-07 19:50:33 -0400

    build out signup form

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,91 @@ import dogSilhouette from "../videos/dog-silhouette.mp4"
 // CSS
 import "../styles/pages/index.css"
 
-const IndexPage = () => {
+const IndexPage = ({location}) => {
+
+  let search = {}
+  let searchSplit = location.search.substring(1).split("&")
+
+  for ( let i = 0 ; i < searchSplit.length ; i++ ) {
+    let query = searchSplit[i].split("=")
+    search[query[0]] = query[1]
+  }
+
+  let emailSignUp
+  if (search.signup === "success"){
+
+    emailSignUp = (
+      <div id="email-success" className="hunting-dog-font">
+        <h3 className="hunting-dog-font" style={{fontSize: "3rem"}}>Thanks for signing up!</h3>
+        <p style={{fontSize: "2rem"}}>Check your email to confirm</p>
+      </div>
+    )
+  
+  } else {
+
+    emailSignUp = (
+      <>
+        <h3 className="hunting-dog-font">Enter your email for updates about the tour!</h3>
+        <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
+        <form 
+          action="https://app.convertkit.com/forms/1725009/subscriptions"
+          className="col s12 seva-form formkit-form"
+          method="post"
+          data-sv-form="1725009"
+          data-uid="7614219ba9"
+          data-format="inline"
+          data-version="5"
+          data-options='{"settings":{"after_subscribe":{"action":"redirect","success_message":"Success! Now check your email to confirm your subscription.","redirect_url":"https://huntingdogfilms.com/?signup=success"},"analytics":{"google":null,"facebook":null,"segment":null,"pinterest":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form"},"recaptcha":{"enabled":false},"return_visitor":{"action":"show","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
+        >
+          <div className="row">
+            <div id="email-signup-form">
+              {/* <h3 className="hunting-dog-font">Enter your email for updates about the tour!</h3> */}
+              <ul class="formkit-alert formkit-alert-error" data-element="errors" data-group="alert"></ul>
+              <div data-element="fields" class="seva-fields formkit-fields">
+                <div 
+                  className="input-field col s6 offset-s2 formkit-field"
+                  style={{marginTop: 0}}
+                >
+                  <input 
+                    id="email" 
+                    type="email" 
+                    className="validate hunting-dog-font formkit-input"
+                    name="email_address"
+                    required 
+                    style={{
+                      color: "white" ,
+                      fontSize: "3rem"
+                    }}/>
+                  <label 
+                    htmlFor="email" 
+                    className="hunting-dog-font"
+                    style={{
+                      fontSize: "1rem"
+                    }}
+                  >
+                    Enter your email
+                  </label>
+                </div>
+                <div className="col s2">
+                  <button 
+                    className="btn waves-effect waves-light" 
+                    data-element="submit"
+                    // form="film-submission-form" 
+                    // type="submit" 
+                    // name="action"
+                  >
+                    Submit
+                    <i className="material-icons right">send</i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </>
+    )
+
+  }
 
   return (  
     <>
@@ -52,28 +136,7 @@ const IndexPage = () => {
 
           {/* ENTER EMAIL FOR UPDATES */}
           <div className="row">
-            <form className="col s12">
-              <div className="row">
-                <h3 className="hunting-dog-font">Enter your email for updates about the tour!</h3>
-                <div className="input-field col s12">
-                  <input 
-                    id="email" 
-                    type="email" 
-                    className="validate hunting-dog-font" 
-                    style={{
-                      color: "white" ,
-                      fontSize: "3rem"
-                    }}/>
-                  <label 
-                    htmlFor="email" 
-                    className="hunting-dog-font"
-                    style={{
-                      fontSize: "1rem"
-                    }}
-                  >Enter your email</label>
-                </div>
-              </div>
-            </form>
+            {emailSignUp}
           </div>
 
         </div>
